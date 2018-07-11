551630acd7dabf6e2ce175fa5206c3dbdfd3be99
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-11 23:35:44 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,6 +21,7 @@ def create_project_hash
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta li a span.location-name").text,
       :percentage_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", " ").to_i
+     binding.pry
     }
   end
 
