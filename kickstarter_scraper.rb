84fd768afd1abd369d070f11c14d3bab33311b44
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-11 22:11:35 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules here
 require 'nokogiri'
+require 'pry'
 
 
 def create_project_hash
@@ -9,4 +10,11 @@ def create_project_hash
   binding.pry
 
   # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  #project.css("ul.project-meta li a span.location-name").text
 end
+
+
+create_project_hash
